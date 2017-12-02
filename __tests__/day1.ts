48eef4c1b7d43de6e86446d917613825710a9564
Christian Roy <christian.roy@gmail.com>
2017-12-02 08:32:35 -0500

    day 1 part 1

--- __tests__/day1.ts
+++ __tests__/day1.ts
@@ -1,4 +1,4 @@
-import {ring} from "../src/day1";
+import {part1, ring} from "../src/day1";
 
 describe("ring", () => {
   test("returns index value when index is within bounds", () => {
@@ -11,5 +11,23 @@ describe("ring", () => {
 
   test("returns appropriate entry value when index is negative", () => {
     expect(ring([1, 2, 3])(-1)).toBe(3);
+  });
+});
+
+describe("part 1", () => {
+  test("1122 should be 3", () => {
+    expect(part1("1122")).toBe(3);
+  });
+
+  test("1111 should be 4", () => {
+    expect(part1("1111")).toBe(4);
+  });
+
+  test("1234 should be 0", () => {
+    expect(part1("1234")).toBe(0);
+  });
+
+  test("91212129 should be 9", () => {
+    expect(part1("91212129")).toBe(9);
   });
 });
--- src/day1.ts
+++ src/day1.ts
@@ -1,5 +1,27 @@
+import * as R from "ramda";
+
 export const ring = (collection: number[]) => (index: number) => {
   const moduloIndex = index % collection.length;
   const inboundsIndex = moduloIndex >= 0 ? moduloIndex : moduloIndex + collection.length;
   return collection[inboundsIndex];
 };
+
+const convertToNumber: (x: string) => number = R.unary(parseInt);
+
+const convertToPairs = (pairIndexCalc) => (numbers: number[]) => {
+  const r = ring(numbers);
+  const range = R.range(0, R.length(numbers));
+  return R.pipe(
+    R.length,
+    R.range(0),
+    R.map((i) => R.pair(r(i), r(pairIndexCalc(i)))),
+    R.filter(([x,y]) => x === y)
+  )(numbers);
+};
+
+export const part1 = R.pipe(
+  R.split(""),
+  R.map(convertToNumber),
+  convertToPairs(R.add(1)),
+  R.map(R.path([0])),
+  R.sum);
