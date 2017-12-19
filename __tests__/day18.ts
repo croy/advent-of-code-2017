a3337e4824bfee309d390ad71f634268a4c387ec
Christian Roy <christian.roy@gmail.com>
2017-12-19 16:52:28 -0500

    day 18 part 2

--- __tests__/day18.ts
+++ __tests__/day18.ts
@@ -1,10 +1,14 @@
 import * as fs from "fs";
-import {part1} from "../src/day18";
+import {part1, part2} from "../src/day18";
 
 describe("sample inputs", () => {
-  test("part1 should be 4", () => {
+  test.skip("part1 should be 4", () => {
     expect(part1(fs.readFileSync("day18-sample.txt", "utf-8").split("\n"))).toBe(4);
+  });
+  test("part2 should be 3", () => {
+    expect(part2(fs.readFileSync("day18-sample-2.txt", "utf-8").split("\n"))).toBe(3);
   });
 });
 
-console.log(part1(fs.readFileSync("day18-input.txt", "utf-8").split("\n")));
+//console.log(part1(fs.readFileSync("day18-input.txt", "utf-8").split("\n")));
+console.log(part2(fs.readFileSync("day18-input.txt", "utf-8").split("\n")));
--- src/day18.ts
+++ src/day18.ts
@@ -1,4 +1,70 @@
 import * as _ from "lodash";
+import * as fs from "fs";
+
+function* program(instructions: string[], programId: number, rcvQ: number[], sndQ: number[]) {
+  const registers: {[index: string]: number} = {p: programId};
+  let instructionPtr = 0;
+  let totalSent = 0;
+
+  while (instructionPtr < instructions.length) {
+    const [instruction, X, Y] = instructions[instructionPtr].split(" ");
+    const Xval = /\d/.test(X) ? parseInt(X, 10) : _.get(registers, X, 0);
+    const Yval = Y && (/\d/.test(Y) ? parseInt(Y, 10) : _.get(registers, Y, 0));
+    switch (instruction) {
+      case "jgz":
+        if (Xval > 0) {
+          instructionPtr += (Yval as number) - 1;
+        }
+        break;
+      case "snd":
+        sndQ.push(Xval);
+        totalSent++;
+        break;
+      case "set":
+        _.set(registers, X, Yval);
+        break;
+      case "add":
+        _.set(registers, X, Xval + (Yval as number));
+        break;
+      case "mul":
+        _.set(registers, X, Xval * (Yval as number));
+        break;
+      case "mod":
+        _.set(registers, X, Xval % (Yval as number));
+        break;
+      case "rcv":
+        if (_.isEmpty(rcvQ)) {
+          yield totalSent;
+        }
+        if (_.isEmpty(rcvQ)) {
+          return totalSent;
+        }
+        const read = rcvQ.shift();
+        _.set(registers, X, read);
+        break;
+    }
+    instructionPtr++;
+  }
+  return totalSent;
+}
+
+export const part2 = (instructions: string[]): number => {
+  const channelA: number[] = [];
+  const channelB: number[] = [];
+  const duetA = program(instructions, 0, channelA, channelB);
+  const duetB = program(instructions, 1, channelB, channelA);
+  let currentA = duetA.next();
+  let currentB = duetB.next();
+  while (!currentA.done || !currentB.done) {
+    if (!currentA.done) {
+      currentA = duetA.next();
+    }
+    if (!currentB.done) {
+      currentB = duetB.next();
+    }
+  }
+  return currentB.value;
+};
 
 export const part1 = (instructions: string[]): number | null => {
   const registers: {[index: string]: number} = {};
@@ -11,7 +77,7 @@ export const part1 = (instructions: string[]): number | null => {
 
     switch (instruction) {
       case "jgz":
-        if (Xval !== 0) {
+        if (Xval > 0) {
           instructionPtr += (Yval as number);
           continue;
         }
