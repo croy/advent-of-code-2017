414f3a08047f7166e448f6d7ed96bfd5874e9b2c
Christian Roy <christian.roy@gmail.com>
2017-12-20 19:59:39 -0500

    day 20 part 2

--- __tests__/day20.ts
+++ __tests__/day20.ts
@@ -1,11 +1,16 @@
 import * as fs from "fs";
-import {part1} from "../src/day20";
+import {part1, part2} from "../src/day20";
 
 describe("samples", () => {
-  const input = fs.readFileSync("day20-sample.txt", "utf-8").split("\n");
+  const sample1 = fs.readFileSync("day20-sample.txt", "utf-8").split("\n");
+  const sample2 = fs.readFileSync("day20-sample-2.txt", "utf-8").split("\n");
 
   test("part1 should be 0", () => {
-    expect(part1(input)).toBe(0);
+    expect(part1(sample1)).toBe(0);
+  });
+
+  test("part2 should be 1", () => {
+    expect(part2(sample2)).toBe(1);
   });
 });
 
@@ -14,5 +19,9 @@ describe("actuals", () => {
 
   test("part 1 should be 243", () => {
     expect(part1(input)).toBe(243);
+  });
+
+  test("part 2 should give an answer", () => {
+    console.log(part2(input));
   });
 });
--- src/day20.ts
+++ src/day20.ts
@@ -1,4 +1,5 @@
 import * as _ from "lodash";
+import { parse } from "path";
 
 interface IAxis {
   pos: number;
@@ -39,7 +40,7 @@ const parseParticle = (particleDatum: string, id: number): IParticle => {
   };
 };
 
-export const part1 = (particleData: string[]): number | undefined => {
+export const part1 = (particleData: string[]): number => {
   const particles = _.map(particleData, parseParticle);
   return _(particles)
   .sortBy([
@@ -48,4 +49,81 @@ export const part1 = (particleData: string[]): number | undefined => {
     (p: IParticle) => p.x.pos + p.y.pos + p.z.pos,
   ])
   .get([0, "id"], 0);
+};
+
+const axisCollision = (axis1: IAxis, axis2: IAxis): number[] | "ALL" => {
+  const a = (axis1.acc - axis2.acc) / 2;
+  const b = axis1.vel - axis2.vel + a;
+  const c = axis1.pos - axis2.pos;
+
+  if (a === 0) {
+    //linear
+    if (b === 0) {
+      if (c === 0) {
+        return "ALL";
+      }
+      return [];
+    }
+    return [-c / b];
+  } else {
+    //quadratic
+    const term = (b * b) - (4 * a * c);
+    if (term < 0) {
+      //imaginary solutions only
+      return [];
+    }
+    return [(-b + Math.sqrt(term)) / (2 * a), (-b - Math.sqrt(term)) / (2 * a)];
+  }
+};
+
+const collisionTime = (particle1: IParticle, particle2: IParticle): number | undefined => {
+  const axisCollisionTimes = _([
+    axisCollision(particle1.x, particle2.x),
+    axisCollision(particle1.y, particle2.y),
+    axisCollision(particle1.z, particle2.z),
+  ])
+  .filter((collisionTimes) => collisionTimes !== "ALL")
+  .value() as number[][];
+
+  if (_.isEmpty(axisCollisionTimes)) {
+    // All axis collide all the time, so time 0 is the first collision
+    return 0;
+  }
+  const particleCollisionTimes = _.intersection(...axisCollisionTimes);
+
+  return _(particleCollisionTimes)
+    .filter(_.isInteger)
+    .filter((t) => t >= 0)
+    .sort()
+    .first();
+};
+
+export const part2 = (particleData: string[]): number => {
+  const particles = _.map(particleData, parseParticle);
+  const collisions: Array<{time: number, id1: number, id2: number}> = [];
+  const destroyed: {[index: number]: number} = {};
+
+  for (let i = 0; i < particles.length - 1; i++) {
+    const particle1 = particles[i];
+
+    for (let j = i + 1; j < particles.length; j++) {
+      const particle2 = particles[j];
+      const collision = collisionTime(particle1, particle2);
+
+      if (!_.isUndefined(collision)) {
+        collisions.push({id1: particle1.id, id2: particle2.id, time: collision});
+      }
+    }
+  }
+
+  _(collisions)
+  .sortBy("time")
+  .forEach((collision) => {
+    if (destroyed[collision.id1] !== collision.time || destroyed[collision.id2] !== collision.time) {
+      destroyed[collision.id1] = collision.time;
+      destroyed[collision.id2] = collision.time;
+    }
+  });
+
+  return particles.length - _.keys(destroyed).length;
 };
