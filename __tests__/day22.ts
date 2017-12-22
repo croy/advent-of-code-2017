6b1bc946e723259aaa7a4278857b8fedffbd783f
Christian Roy <christian.roy@gmail.com>
2017-12-22 10:19:11 -0500

    day 22 part 2

--- __tests__/day22.ts
+++ __tests__/day22.ts
@@ -1,5 +1,5 @@
 import * as fs from "fs";
-import {part1} from "../src/day22";
+import {part1, part2} from "../src/day22";
 
 describe("sample data", () => {
   const sample = fs.readFileSync("day22-sample.txt", "utf-8").split("\n");
@@ -8,13 +8,20 @@ describe("sample data", () => {
     expect(part1(sample).infected).toBe(5587);
   });
 
+  test("part2 should be 2511944", () => {
+    expect(part2(sample).infected).toBe(2511944);
+  });
+
 });
 
 describe("actual data", () => {
   const input = fs.readFileSync("day22-input.txt", "utf-8").split("\n");
 
   test("part1", () => {
-    console.log(part1(input).infected);
+    console.log(`part1 : ${part1(input).infected}`);
   });
 
+  test("part2", () => {
+    console.log(`part2 : ${part2(input).infected}`);
+  });
 });
--- src/day22.ts
+++ src/day22.ts
@@ -1,70 +1,98 @@
 import * as _ from "lodash";
 
-const UP = {
-  name: "UP",
-  x: 0,
-  y: -1,
-};
+const UP: IDirection = {name: "UP", x: 0, y: -1};
+const DOWN: IDirection = {name: "DOWN", x: 0, y: 1};
+const LEFT: IDirection = {name: "LEFT", x: -1, y: 0};
+const RIGHT: IDirection = {name: "RIGHT", x: 1, y: 0};
 
-const DOWN = {
-  name: "DOWN",
-  x: 0,
-  y: 1,
-};
+const rightTurn: ITurn = {DOWN: LEFT, LEFT: UP, RIGHT: DOWN, UP: RIGHT};
+const leftTurn: ITurn = {DOWN: RIGHT, LEFT: DOWN, RIGHT: UP, UP: LEFT};
+const forward: ITurn = {UP, LEFT, DOWN, RIGHT};
+const reverse: ITurn = {UP: DOWN, LEFT: RIGHT, DOWN: UP, RIGHT: LEFT};
 
-const LEFT = {
-  name: "LEFT",
-  x: -1,
-  y: 0,
-};
+interface IDirection {
+  name: string;
+  x: -1 | 0 | 1;
+  y: -1 | 0 | 1;
+}
 
-const RIGHT = {
-  name: "RIGHT",
-  x: 1,
-  y: 0,
-};
+interface ITurn {
+  [index: string]: IDirection;
+}
 
-const rightTurn = {
-  UP: RIGHT,
-  RIGHT: DOWN,
-  DOWN: LEFT,
-  LEFT: UP
-};
+interface IRule {
+  next: string;
+  action: string;
+  nextDirLookup: ITurn;
+}
 
-const leftTurn = {
-  UP: LEFT,
-  LEFT: DOWN,
-  DOWN: RIGHT,
-  RIGHT: UP
-};
-
-export const part1 = (input: string[], steps = 10000) => {
-  const grid = input.map((line) => line.split(""));
-  let currentPos = {
+const simulate = (grid: string[][], steps: number, rules: {[index: string]: IRule}) => {
+  const currentPos = {
     x: Math.floor(grid.length / 2),
     y: Math.floor(grid.length / 2),
   };
+  const result: {[index: string]: number} = _(rules)
+  .values()
+  .map("action")
+  .reduce((acc, val) => _.set(acc, val, 0), {});
+
   let currentDirection = UP;
-  const result = {
-    cleaned: 0,
-    infected: 0,
-  };
 
   for (let i = 0; i < steps; i++) {
     const currentSquare = _.get(grid, [currentPos.y, currentPos.x], ".");
-    if (currentSquare === ".") {
-      _.set(grid, [currentPos.y, currentPos.x], "#");
-      result.infected++;
-      currentDirection = leftTurn[currentDirection.name];
-    } else {
-      _.set(grid, [currentPos.y, currentPos.x], ".");
-      result.cleaned++;
-      currentDirection = rightTurn[currentDirection.name];
-    }
-    //console.log(`${currentPos.x},${currentPos.y}: ${currentSquare} -> ${currentDirection.name}`);
+    const rule = rules[currentSquare];
+
+    _.set(grid, [currentPos.y, currentPos.x], rule.next);
+
+    currentDirection = rule.nextDirLookup[currentDirection.name];
+
+    result[rule.action]++;
+
     currentPos.x += currentDirection.x;
     currentPos.y += currentDirection.y;
   }
 
   return result;
+};
+
+export const part1 = (input: string[], steps = 10000) => {
+  const grid = input.map((line) => line.split(""));
+  return simulate(grid, steps, {
+    "#": {
+      action: "cleaned",
+      next: ".",
+      nextDirLookup: rightTurn,
+    },
+    ".": {
+      action: "infected",
+      next: "#",
+      nextDirLookup: leftTurn,
+    },
+  });
+};
+
+export const part2 = (input: string[], steps = 10000000) => {
+  const grid = input.map((line) => line.split(""));
+  return simulate(grid, steps, {
+    "#": {
+      action: "flagged",
+      next: "f",
+      nextDirLookup: rightTurn,
+    },
+    ".": {
+      action: "weakened",
+      next: "w",
+      nextDirLookup: leftTurn,
+    },
+    "f": {
+      action: "cleaned",
+      next: ".",
+      nextDirLookup: reverse,
+    },
+    "w": {
+      action: "infected",
+      next: "#",
+      nextDirLookup: forward,
+    },
+  });
 };
